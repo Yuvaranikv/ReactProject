f7025e07fa406140e1288d2e8fb555fedb292285
yuvaranik <yuvaranikv@gmail.com>
2024-06-01 17:18:16 +0530

    Changed todolist

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, act, useReducer } from 'react';
+import React, { useState, useEffect, useReducer } from 'react';
 import ToDo from './ToDo';
 
 const initialState = { todos: [] };
@@ -22,28 +22,31 @@ const todoReducer = (state, action) => {
 };
 
 const ToDoList = () => {
-    const [input, setInput] = useState('');
+    const [input, setInput] = useState(''); // Initialize input state with an empty string
     const [state, dispatch] = useReducer(todoReducer, initialState);
     const { todos } = state;
 
     const handleAdd = () => {
         if (!input) return;
         dispatch({ type: 'ADD_TODO', payload: { taskName: input, completed: false } });
-        setInput('');
-      };
+        setInput(''); // Clear input after adding todo
+    };
 
     const handleRemove = (index) => {
         dispatch({ type: 'REMOVE_TODO', payload: index });
-  };
+    };
 
     const handleToggle = (index) => {
         dispatch({ type: 'TOGGLE_TODO', payload: index });
     };
+
     const handleChange = (event) => {
         setInput(event.target.value);
-      };
+    };
+    const handleReset = () => {
+      dispatch({ type: 'RESET_LIST' });
+  };
 
-    
 
     useEffect(() => {
         localStorage.setItem('todos', JSON.stringify(todos));
@@ -51,23 +54,27 @@ const ToDoList = () => {
 
     return (
         <div>
-      <h1>To-do list Exercise</h1>
-      <input value={input} onChange={handleChange} />
-      <button onClick={handleAdd}>Add</button>
+            <h1>To-do list Exercise</h1>
+            <input value={input} onChange={handleChange} />
+            <button onClick={handleAdd}>Add</button>
+            <button onClick={handleReset}>Reset List</button>
 
-      <ul>
-        {todos.map((todo, index) => (
-          <ToDo
-            key={index}
-            index={index}
-            todo={todo}
-            handleToggle={handleToggle}
-            handleRemove={handleRemove}
-          />
-        ))}
-      </ul>
-    </div>
-  );
+            <ul>
+            
+                {todos.map((todo, index) => (
+                    <ToDo
+                        key={index}
+                        index={index}
+                        task={todo} // Changed 'todo' to 'task' to match the prop name in ToDo component
+                        handleToggle={handleToggle}
+                        handleRemove={handleRemove}
+                       
+                    />
+                   
+                ))}
+            </ul>
+        </div>
+    );
 };
 
 export default ToDoList;
